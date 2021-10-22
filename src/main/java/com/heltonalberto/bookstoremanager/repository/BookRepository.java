43ebe9f24c17698d298f078f556c7fca05e08241
Helton <heltonalberto@gmail.com>
2021-10-22 12:28:14 -0300

    Create book repository class

--- src/main/java/com/heltonalberto/bookstoremanager/repository/BookRepository.java
+++ src/main/java/com/heltonalberto/bookstoremanager/repository/BookRepository.java
@@ -1,11 +1,8 @@
 package com.heltonalberto.bookstoremanager.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
 import com.heltonalberto.bookstoremanager.entity.Book;
 
-@Repository
 public interface BookRepository extends JpaRepository<Book, Long>{
 
 }
